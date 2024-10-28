c048350a841b813299b2be6f2ab67462a2ee3b3b
Carlos Castillo <castillo.st@gmail.com>
2024-10-28 21:12:41 +0100

    💁‍♀️ Add character sheet struct

---
+++ internal/ui/character_sheet.go
@@ -0,0 +1,16 @@
+package ui
+
+//go:generate gonstructor -type=CharacterSheet -constructorTypes=builder -output=character_sheet_auto.go
+
+// CharacterSheet Is a struct that can render the character sheet in a text format
+type CharacterSheet struct {
+	fullName        string
+	role            string
+	experience      string
+	skills          []string
+	characteristics []string
+}
+
+func (c *CharacterSheet) Render() string {
+	return "Character Sheet"
+}
