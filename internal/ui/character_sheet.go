83826e53578dd2ad3f21049466d3de5b508cfe4c
Carlos Castillo <castillo.st@gmail.com>
2024-10-28 21:53:41 +0100

    🎨 Draft a first sheet example

--- internal/ui/character_sheet.go
+++ internal/ui/character_sheet.go
@@ -1,5 +1,12 @@
 package ui
 
+import (
+	"fmt"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
+	"strings"
+)
+
 //go:generate gonstructor -type=CharacterSheet -constructorTypes=builder -output=character_sheet_auto.go
 
 // CharacterSheet Is a struct that can render the character sheet in a text format
@@ -12,5 +19,31 @@ type CharacterSheet struct {
 }
 
 func (c *CharacterSheet) Render() string {
-	return "Character Sheet"
+	headers := []string{"Attribute", "Value"}
+	data := [][]string{
+		{"Full Name", c.fullName},
+		{"Role", c.role},
+		{"Experience", c.experience},
+		{"Skills", strings.Join(c.skills, ", ")},
+	}
+
+	for characteristic, value := range c.characteristics {
+		data = append(data, []string{characteristic.String(), fmt.Sprintf("%d", value)})
+	}
+
+	sheet := table.New().
+		Border(lipgloss.NormalBorder()).
+		Headers(headers...).
+		Rows(data...).
+		Width(50).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			baseStyle := lipgloss.NewStyle().Padding(0, 1)
+			headerStyle := baseStyle.Foreground(lipgloss.Color("252")).Bold(true)
+			if row == 0 {
+				return headerStyle
+			}
+			return baseStyle
+		})
+
+	return sheet.String()
 }
