cf90ba47feb7241aadadee958b412fb08ad1f84c
Carlos Castillo <castillo.st@gmail.com>
2024-11-09 12:02:48 +0100

    🎨 Draft Name / Role / Experience

--- internal/ui/character_sheet.go
+++ internal/ui/character_sheet.go
@@ -9,6 +9,8 @@ import (
 
 //go:generate gonstructor -type=CharacterSheet -constructorTypes=builder -output=character_sheet_auto.go
 
+const headerIndex = -1
+
 // CharacterSheet Is a struct that can render the character sheet in a text format
 type CharacterSheet struct {
 	fullName        string
@@ -19,31 +21,57 @@ type CharacterSheet struct {
 }
 
 func (c *CharacterSheet) Render() string {
-	headers := []string{"Attribute", "Value"}
-	data := [][]string{
-		{"Full Name", c.fullName},
-		{"Role", c.role},
-		{"Experience", c.experience},
-		{"Skills", strings.Join(c.skills, ", ")},
-	}
+	sheetRender := new(strings.Builder)
 
-	for characteristic, value := range c.characteristics {
-		data = append(data, []string{characteristic.String(), fmt.Sprintf("%d", value)})
-	}
+	style := newLipGlossDefaultStyle()
+	titleBox := lipgloss.NewStyle().
+		Bold(true).
+		PaddingLeft(1).
+		Foreground(style.titleColor)
+	valueBox := lipgloss.NewStyle().
+		MarginLeft(1).
+		Foreground(style.valueColor)
 
-	sheet := table.New().
-		Border(lipgloss.NormalBorder()).
-		Headers(headers...).
-		Rows(data...).
-		Width(50).
+	nameRoleTable := table.New().
+		Border(lipgloss.RoundedBorder()).
+		Headers("Name", "Role").
+		Rows([]string{c.fullName, fmt.Sprintf("%v (%v)", c.role, c.experience)}).
+		Width(80).
 		StyleFunc(func(row, col int) lipgloss.Style {
-			baseStyle := lipgloss.NewStyle().Padding(0, 1)
-			headerStyle := baseStyle.Foreground(lipgloss.Color("252")).Bold(true)
-			if row == 0 {
-				return headerStyle
+			switch row {
+			case headerIndex:
+				return titleBox
+			default:
+				return valueBox
 			}
-			return baseStyle
 		})
 
-	return sheet.String()
+	sheetRender.WriteString(nameRoleTable.Render())
+	//headers := []string{"Attribute", "Value"}
+	//data := [][]string{
+	//	{"Full Name", c.fullName},
+	//	{"Role", c.role},
+	//	{"Experience", c.experience},
+	//	{"Skills", strings.Join(c.skills, ", ")},
+	//}
+	//
+	//for characteristic, value := range c.characteristics {
+	//	data = append(data, []string{characteristic.String(), fmt.Sprintf("%d", value)})
+	//}
+	//
+	//sheet := table.New().
+	//	Border(lipgloss.RoundedBorder()).
+	//	Headers(headers...).
+	//	Rows(data...).
+	//	Width(50).
+	//	StyleFunc(func(row, col int) lipgloss.Style {
+	//		baseStyle := lipgloss.NewStyle().Padding(0, 1)
+	//		headerStyle := baseStyle.Foreground(lipgloss.Color("252")).Bold(true)
+	//		if row == 0 {
+	//			return headerStyle
+	//		}
+	//		return baseStyle
+	//	})
+
+	return sheetRender.String()
 }
