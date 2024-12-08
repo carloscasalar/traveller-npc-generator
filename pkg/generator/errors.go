fb854cefaeeeff8b653f098fada2c43481e1f0a0
Carlos Castillo <castillo.st@gmail.com>
2024-12-08 13:26:10 +0100

    🪄 Replace generated errors with handmade error

--- pkg/generator/errors.go
+++ pkg/generator/errors.go
@@ -1,6 +1,15 @@
 package generator
 
-//go:generate errgen -type=generatorErrors --out-file=errors_auto.go
-type generatorErrors struct {
-	EmptyListErr error `errmsg:"invalid %v list: should contain at least one item" vars:"listName string"`
+import "fmt"
+
+type InvalidListError struct {
+	listName string
+}
+
+func newInvalidListError(listName string) InvalidListError {
+	return InvalidListError{listName: listName}
+}
+
+func (e InvalidListError) Error() string {
+	return fmt.Sprintf("invalid list %s, a non-empty list must be provided", e.listName)
 }
--- tools.go
+++ tools.go
@@ -5,6 +5,5 @@ package main
 
 import (
 	_ "github.com/alvaroloes/enumer"
-	_ "github.com/moznion/go-errgen/cmd/errgen"
 	_ "github.com/moznion/gonstructor/cmd/gonstructor"
 )
